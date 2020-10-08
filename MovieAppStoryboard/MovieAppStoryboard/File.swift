f272d68f123cb01a39c99c71cbb06b5675af0258
jhjang <jhjang@10.11.52.125>
2020-10-08 10:22:37 +0900

    앱 구성을 위한 초기 커밋

---
+++ MovieAppStoryboard/MovieAppStoryboard/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MovieAppStoryboard
+//
+//  Created by AKIS-iMac on 2020/10/07.
+//
+
+import Foundation
---
+++ MovieAppStoryboard/MovieAppStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppStoryboard
+//
+//  Created by AKIS-iMac on 2020/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
