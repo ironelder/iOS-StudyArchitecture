1833252e485633058c13749644e1ab11a7c1ed44
jhjang <jhjang@10.11.52.125>
2020-10-15 17:51:37 +0900

    일반 코딩으로 앱 추가 설정

---
+++ NoStoryBoardApp/NoStoryBoardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryBoardApp
+//
+//  Created by AKIS-iMac on 2020/10/15.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
