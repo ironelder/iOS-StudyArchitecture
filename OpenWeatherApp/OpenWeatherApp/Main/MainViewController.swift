ab5bf1890c45279b0420ca251840a432dea36bb2
jhjang <jhjang@10.11.52.125>
2020-12-22 13:48:41 +0900

    날씨 얻어오기 위한 위도 경도 계산 방식 변경

--- OpenWeatherApp/OpenWeatherApp/Main/MainViewController.swift
+++ OpenWeatherApp/OpenWeatherApp/Main/MainViewController.swift
@@ -14,13 +14,9 @@ class MainViewController: UIViewController {
     private let locationManager = CLLocationManager()
     private let apikey = "c88eb92a38e25476371cc3c795b57ada"
     
-    private var weatherLatitude:CLLocationDegrees = 0.0
-    private var weatherLongitude:CLLocationDegrees = 0.0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         initializedLocation()
-        getWeatherData()
     }
     
     private func initializedLocation() {
@@ -29,19 +25,15 @@ class MainViewController: UIViewController {
         locationManager.requestAlwaysAuthorization()
         if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
             locationManager.startUpdatingLocation()
-            if let currentLocation = locationManager.location {
-                weatherLatitude = currentLocation.coordinate.latitude
-                weatherLongitude = currentLocation.coordinate.longitude
-            }
         }
     }
     
-    private func getWeatherData() {
+    private func getWeatherData(_ lat:CLLocationDegrees, _ lon:CLLocationDegrees) {
         let headers: HTTPHeaders = [
 //            "Authorization": "Basic VXNlcm5hbWU6UGFzc3dvcmQ=",
             "Accept": "application/json"
         ]
-        AF.request("https://api.openweathermap.org/data/2.5/weather?lat=\(weatherLatitude)&lon=\(weatherLongitude)&appid=\(apikey)", headers: headers).responseJSON { response in
+        AF.request("https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apikey)", headers: headers).responseJSON { response in
             switch response.result {
                 case.success(let result):
                     print("result = \(result)")
@@ -59,6 +51,7 @@ extension MainViewController: CLLocationManagerDelegate {
         let location = locations[0] as CLLocation
         print("locationManager latitude = \(String(describing: location.coordinate.latitude))")
         print("locationManager longitude = \(String(describing: location.coordinate.longitude))")
+        getWeatherData(location.coordinate.latitude, location.coordinate.longitude)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
